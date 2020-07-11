70929c9ba8143ce9435c1cf8b849d87e45b5f470
BrayanDevM <bdevia95@gmail.com>
2020-07-11 11:45:06 -0500

    -bootsrap-select +@ng-select

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Módulos externos
@@ -50,6 +51,7 @@ import { BusquedaComponent } from './busqueda/busqueda.component';
     CommonModule,
     PagesRoutingModule,
     ComponentsModule,
+    NgSelectModule,
     FormsModule,
     ReactiveFormsModule
   ],
