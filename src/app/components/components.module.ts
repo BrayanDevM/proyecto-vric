f9e61bec8b24bd2faf1a78506c2dc5bb4ddae1f2
BrayanDevM <bdevia95@gmail.com>
2020-09-07 20:24:18 -0500

    Eliminados componentes innecesarios

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,43 +6,35 @@ import { RouterModule } from '@angular/router';
 import { ChartsModule } from 'ng2-charts';
 import { NgApexchartsModule } from 'ng-apexcharts';
 
+// Módulos
+import { MaterialModule } from '../material/material.module';
+import { AppPipesModule } from '../pipes/app-pipes.module';
+
 // Componentes
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { TablaBeneficiariosComponent } from './tabla-beneficiarios/tabla-beneficiarios.component';
+import { DialogAcercaDeComponent } from './dialogs/dialog-acerca-de/dialog-acerca-de.component';
 import { FormIngresosComponent } from './form-ingresos/form-ingresos.component';
 import { FormEgresosComponent } from './form-egresos/form-egresos.component';
 import { FormCambiosComponent } from './form-cambios/form-cambios.component';
-import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
-import { InfoBeneficiarioComponent } from './modals/info-beneficiario/info-beneficiario.component';
-import { LineChartComponent } from './charts/line-chart/line-chart.component';
-import { ModalActualizacionesComponent } from './modals/modal-actualizaciones/modal-actualizaciones.component';
-import { DialogAcercaDeComponent } from './dialogs/dialog-acerca-de/dialog-acerca-de.component';
-import { MaterialModule } from '../material/material.module';
+import { DialogFormIngresoComponent } from './dialogs/dialog-form-ingreso/dialog-form-ingreso.component';
 import { ApexPieChartComponent } from './apexchart/apex-pie-chart/apex-pie-chart.component';
 import { ApexLineChartTimeseriesComponent } from './apexchart/apex-line-chart-timeseries/apex-line-chart-timeseries.component';
 import { ApexLineChartMinComponent } from './apexchart/apex-line-chart-min/apex-line-chart-min.component';
 import { ApexBarChartStackedComponent } from './apexchart/apex-bar-chart-stacked/apex-bar-chart-stacked.component';
-import { AppPipesModule } from '../pipes/app-pipes.module';
-import { DialogFormIngresoComponent } from './dialogs/dialog-form-ingreso/dialog-form-ingreso.component';
 
 const components = [
   HeaderComponent,
   SidebarComponent,
-  TablaBeneficiariosComponent,
+  DialogAcercaDeComponent,
   FormIngresosComponent,
   FormEgresosComponent,
   FormCambiosComponent,
-  PieChartComponent,
-  InfoBeneficiarioComponent,
-  LineChartComponent,
-  ModalActualizacionesComponent,
-  DialogAcercaDeComponent,
+  DialogFormIngresoComponent,
   ApexPieChartComponent,
   ApexLineChartMinComponent,
   ApexLineChartTimeseriesComponent,
-  ApexBarChartStackedComponent,
-  DialogFormIngresoComponent
+  ApexBarChartStackedComponent
 ];
 
 @NgModule({
